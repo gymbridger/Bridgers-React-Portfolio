82517beda4c59f85f8acb8e814c5c28439276f1d
Bridger Daw <bridgerdaw@yahoo.com>
2023-09-20 19:41:07 -0600

    switched to hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import AboutMe from "./components/aboutme/AboutMe";
 import Portfolio from "./components/portfolio/Portfolio";
